70dc26eafefab9505a6868d377005f2eda8a1a03
Alex Drennen <drennen42@gmail.com>
2015-01-22 17:34:37 -0600

    complete overhaul

---
+++ Resume/javascripts/examples.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("#demosMenu").change(function(){
+    window.location.href = $(this).find("option:selected").attr("id") + '.html';
+  });
+
+  var t = setInterval(function(){
+    $("#carousel ul").animate({marginLeft:-480},1000,function(){
+      $(this).find("li:last").after($(this).find("li:first"));
+      $(this).css({marginLeft:0});
+    })
+  },5000);
+
+});
